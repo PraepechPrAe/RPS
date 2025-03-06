2369c1dd1eeea810b61c570f02a4e494abaa4a75
PraepechPrAe <praepech.t@gmail.com>
2025-03-06 20:51:45 +0700

    restrict players

--- RPSLS.sol
+++ RPSLS.sol
@@ -20,6 +20,12 @@ contract RPS is CommitReveal, TimeUnit{
     uint public numReveal = 0;
     uint public expired_time = 10 minutes;
 
+    address[] public allowedPlayers = [
+        0x5B38Da6a701c568545dCfcB03FcB875f56beddC4,
+        0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2,
+        0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db,
+        0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB
+    ];
 
     function addPlayer() public payable {
         require(numPlayer < 2);
@@ -100,7 +106,6 @@ contract RPS is CommitReveal, TimeUnit{
         address payable account0 = payable(player[0].addr);
         address payable account1 = payable(player[1].addr);
         
-
         if (p0Choice == p1Choice) {
         // Tie case: Split the reward
         account0.transfer(reward / 2);
